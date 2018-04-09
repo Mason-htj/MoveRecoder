447ed4c6eff7cd74a1174968800850111b79b712
Hong Desktop <htj889@naver.com>
2018-04-09 22:09:47 +0900

    [#1] Base들 수정

--- app/src/main/java/com/hong/mason/moverecoder/base/BaseFragment.kt
+++ app/src/main/java/com/hong/mason/moverecoder/base/BaseFragment.kt
@@ -9,13 +9,11 @@ import android.view.ViewGroup
 abstract class BaseFragment : Fragment() {
     abstract fun getLayoutResourceId(): Int
     abstract fun initView(view: View)
-    abstract fun initArguments(args: Bundle)
+    abstract fun initArguments(args: Bundle?)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (arguments != null) {
-            initArguments(arguments)
-        }
+        initArguments(arguments)
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
--- app/src/main/java/com/hong/mason/moverecoder/base/BaseTabFragment.kt
+++ app/src/main/java/com/hong/mason/moverecoder/base/BaseTabFragment.kt
@@ -1,7 +1,5 @@
 package com.hong.mason.moverecoder.base
 
-import android.support.v4.app.Fragment
-
 abstract class BaseTabFragment : BaseFragment() {
     abstract fun getTitle(): String
 }
